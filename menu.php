e4d7d740da1b7c579aaaf75f564ce53b8909700a
jaime <jaime.galindo@campusfp.es>
2019-02-06 10:05:46 +0100

    menu.php
    estructura menu

---
+++ menu.php
@@ -0,0 +1,27 @@
+<?php
+    echo '
+        <center><h1 style="border-bottom: solid 1px black; text-align:center; margin-left: 35%; margin-right: 35%; font-size: 500%;">MENÚ</h1>
+        <form action="" method="POST">
+            <input type="submit" name="submit" value="Tres En Raya" style="width: 40%; height: 100px; font-size: 2em;">
+        </form>
+        <br> <br>
+        <form action="" method="POST">
+            <input type="submit" name="submit" value="Camara Secreta" style="width: 40%; height: 100px; font-size: 2em;">
+        </form>
+        <br> <br>
+        <form action="" method="POST">
+            <input type="submit" name="submit" value="Calculadora" style="width: 40%; height: 100px; font-size: 2em;">
+        </form>
+        <br> <br> <br>
+        Sergio Nebreda Andrés
+        <br> <br>
+        Pedro Heras Sánchez
+        <br> <br>
+        Victor Cuadrillero Galán
+        <br> <br>
+        Jorge Mayo Peñalver
+        <br> <br>
+        Jaime Galindo del Arco
+        
+    ';
+?>
